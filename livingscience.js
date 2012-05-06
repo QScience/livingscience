f3375c5cfee6597c9343208f1fa7a8c2daf48da6
Vahe Tshitoyan <tvahe@student.ethz.ch>
2012-05-06 17:28:44 +0200

    pagination on the list

--- livingscience.js
+++ livingscience.js
@@ -4,6 +4,7 @@
 
 var lsObject = new Array();
 var query = new Array();
+var currentPage = new Array();
 
 function lsSearch(idSuffix) {
 
@@ -49,7 +50,11 @@ function lsSearch(idSuffix) {
 		document.getElementById("searchResults-"+idSuffix+"-1").innerHTML = "";
 		lsObject[idSuffix].searchAuthor(query[idSuffix], function(publications) 
 		{
-			var shortInfo =  "<p>" + lsObject[idSuffix].getTotalResults() + " results for: "+query[idSuffix]+"</p>";
+			var numberOfResults = lsObject[idSuffix].getTotalResults();
+			// Create the pagination links
+			makePagination(numberOfResults, 10, idSuffix);
+			
+			var shortInfo =  "<p>" + numberOfResults + " results for: "+query[idSuffix]+"</p>";
 			var authorIndices = lsObject[idSuffix].getAuthorStats();
 			if (authorIndices)
 			{
@@ -59,14 +64,63 @@ function lsSearch(idSuffix) {
 			document.getElementById("watchProgress-"+idSuffix+"-1").innerHTML = shortInfo;
 			
 			// display result list
-			document.getElementById("searchResults-"+idSuffix+"-1").innerHTML = "";
-			lsObject[idSuffix].generateList(0, 10,  "searchResults-"+idSuffix+"-1");
+			lsPage(1, numberOfResults, 10, idSuffix);
+			currentPage[idSuffix] = 1;
+//			lsObject[idSuffix].generateList(0, 10,  "searchResults-"+idSuffix+"-1");
 			
 			// search for an author image
 			lsObject[idSuffix].addAuthorImage("authorImage" + idSuffix, 100); // 100 px is image width
 		});
 	  }
 	});
+}
 
-	
+/**
+ * Function responsible for changing the page
+ * @param pg the page to change to
+ * @param total number of total results
+ * @param perpage how many results to show per page
+ * @param idSuffix the idSuffix of the dialog window
+ */
+function lsPage(pg, total, perpage, idSuffix) {
+	numberOfPages = Math.floor(total/perpage)+1;
+	if (pg == "next") {
+		pg = currentPage[idSuffix] + 1;
+		if (pg > numberOfPages) return false;
+	} else if (pg == "prev") {
+		pg = currentPage[idSuffix] - 1;
+		if (pg == 0) return false;
+	}
+	document.getElementById("searchResults-"+idSuffix+"-1").innerHTML = "";
+	lsObject[idSuffix].generateList((pg-1)*perpage, pg*perpage,  "searchResults-"+idSuffix+"-1");
+	currentPage[idSuffix] = pg;
+	highlightCurrent(pg);
+}
+
+/**
+ * Function responsible for creating the pagination links based on the number of results
+ * and results per page
+ * @param total total number results
+ * @param perpage how many to show per page
+ * @param idSuffix idSuffix of the dialog window
+ */
+function makePagination(total, perpage, idSuffix) {
+	numberOfPages = Math.floor(total/perpage)+1;
+	divId = "ls-pagination-"+idSuffix+"-1";
+	output = "<ul><li style='display: inline; list-style-type: none; padding-right: 5px;' onclick='lsPage(&quot;prev&quot;, "+total+", "+perpage+", "+idSuffix+")'><a class='ls-pagination' href='#'><< prev</a></li>";
+	for (i=1; i <= numberOfPages; i++) {
+		output += "<li style='display: inline; list-style-type: none; padding-right: 5px;' onclick='lsPage("+i+", "+total+", "+perpage+", "+idSuffix+")'><a class='ls-pagination' id='ls-pagination-"+i+"' href='#'>"+i+"</a></li>";		
+	}
+	output += "<li style='display: inline; list-style-type: none; padding-right: 5px;' class='pagination' onclick='lsPage(&quot;next&quot;, "+total+", "+perpage+", "+idSuffix+")'><a class='ls-pagination' href='#'>next >></a></li></ul>";
+	jQuery("#"+divId).html(output);
+}
+
+/**
+ * Function to highlight the currently selected page
+ * @param pg
+ */
+function highlightCurrent(pg) {
+	liId = "ls-pagination-"+pg;
+	jQuery(".ls-pagination").css("text-decoration", "none");
+	jQuery("#"+liId).css("text-decoration", "underline");
 }
